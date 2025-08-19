ab738c8ecc0997af7615bc85c8c1e73aabf252a0
priyanshu050307 <priyanshu.vish2007@gmail.com>
2025-08-19 22:58:46 +0530

    Fix eslint unused vars

--- src/components/RegistrationModal.js
+++ src/components/RegistrationModal.js
@@ -21,7 +21,7 @@ const RegistrationModal = ({ isOpen, onClose }) => {
     setSubmitStatus(null);
 
     try {
-      const res = await axios.post('https://eduweb-backend.onrender.com/api/contact', form);
+      // const res = await axios.post('https://eduweb-backend.onrender.com/api/contact', form);
       setSubmitStatus({ type: 'success', message: 'Registration request sent successfully! We will contact you soon.' });
       setTimeout(() => {
         onClose();
